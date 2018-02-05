93f3f7469bb3e3c825dfa993d44b61edcc548e3d
Erik Erikson <erik.m.erikson@nordstrom.com>
2018-02-05 14:34:35 -0800

    add chai-as-promised for completeness
    doesn't change the behavior.  but it is more consistent with the use of and Error for the rejection which then can run into the root bug here (i.e. see the 'reject-error' branch).  the issue still exists there.

--- test.spec.js
+++ test.spec.js
@@ -1,6 +1,8 @@
 const BbPromise = require('bluebird');
 const chai = require('chai');
 
+chai.use(require('chai-as-promised'))
+
 const expect = chai.expect;
 
 process.on('unhandledRejection', err => {
