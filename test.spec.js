a2b2100619febb982c8cd926b00dbd8318e9d5b9
Erik Erikson <erik.m.erikson@nordstrom.com>
2018-02-05 14:19:17 -0800

    Reject with Error instead

--- test.spec.js
+++ test.spec.js
@@ -1,19 +1,32 @@
 const BbPromise = require('bluebird');
 const chai = require('chai');
 
+chai.use(require('chai-as-promised'))
+
 const expect = chai.expect;
 
 process.on('unhandledRejection', err => {
-    console.log(`UNHANDLED! ${err}`);
-    throw err;
+  console.log(`UNHANDLED! ${err}`);
+  throw err;
 });
 
+class ServerlessError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.name = this.constructor.name;
+    this.message = message;
+    this.statusCode = statusCode;
+    Error.captureStackTrace(this, this.constructor);
+  }
+};
+
 describe('Variables', function variables() {
-    it('simpler', () => {
-        return expect(BbPromise.reduce([
-            BbPromise.resolve('foo'),
-            BbPromise.resolve('bar'),
-            BbPromise.reject('reason')
-        ])).to.be.rejected;
-    })
+  it('simpler', () => {
+    const sls = new Serverless();
+    return expect(BbPromise.reduce([
+      BbPromise.resolve('foo'),
+      BbPromise.resolve('bar'),
+      BbPromise.reject(new ServerlessError('reason'))
+    ])).to.be.rejected;
+  })
 });
